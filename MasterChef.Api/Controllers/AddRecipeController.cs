1414286a17bb544d19c1c507805931f444921102
PAMELA JUSTI PINHEIRO <pamelajusti@yahoo.com.br>
2024-04-05 19:14:10 -0300

    first commit

--- MasterChef.Api/Controllers/AddRecipeController.cs
+++ MasterChef.Api/Controllers/AddRecipeController.cs
@@ -5,34 +5,31 @@ using NuGet.Protocol.Plugins;
 
 namespace MasterChef.Api.Controllers
 {
-    public class CadastrarReceitaController : Controller
+    public class AddRecipeController : Controller
     {
         private readonly IRecipeService _recipeService;
 
-        public CadastrarReceitaController(IRecipeService recipeService)
+        public AddRecipeController(IRecipeService recipeService)
         {
             _recipeService = recipeService;
         }
-        // GET: CadastrarReceitaController
         public ActionResult Index()
         {
-            var recipt = new Recipe();
-            return View(recipt);
+            var recipe = new Recipe();
+
+            return View(recipe);
         }
 
-        // GET: CadastrarReceitaController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: CadastrarReceitaController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: CadastrarReceitaController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(Recipe receipe)
@@ -57,7 +54,6 @@ namespace MasterChef.Api.Controllers
             }
         }
 
-        // GET: CadastrarReceitaController/Edit/5
         public async Task<ActionResult> Edit(int id)
         {
             var recipe = await _recipeService.GetRecipe(id);
@@ -65,7 +61,6 @@ namespace MasterChef.Api.Controllers
             return View(nameof(Index), recipe);
         }
 
-        // POST: CadastrarReceitaController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit(int id, Recipe receipe)
@@ -81,14 +76,12 @@ namespace MasterChef.Api.Controllers
             }
         }
 
-        // GET: CadastrarReceitaController/Delete/5
         public ActionResult Delete(int id)
         {
             _recipeService.Delete(id);
             return RedirectToAction("Index", "Home");
         }
 
-        // POST: CadastrarReceitaController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
